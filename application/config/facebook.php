d2ed113556a97ee852cb30fa3a1e291a5c8c4a53
skymanc <browuchen@gmail.com>
2013-03-22 10:58:24 +0800

    123

---
+++ application/config/facebook.php
@@ -0,0 +1,6 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['facebook_app_id']             = '344617158898614';
+$config['facebook_api_secret']         ='6dc8ac871858b34798bc2488200e503d';
+$config['facebook_default_scope']	= 'email,offline_access,publish_stream,user_birthday,user_location,user_about_me,user_hometown';
+$config['facebook_api_url']         = 'http://apps.facebook.com/loginforskyman';
